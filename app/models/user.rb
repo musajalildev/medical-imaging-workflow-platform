47984520fbee165abd919749c2440c7af4c6fa37
Ethan Wood <ejwood1@sheffield.ac.uk>
2026-03-22 23:28:51 +0000

    Revert "added basic form and job db entries with dummy function for files. However still tries to store in the DB"
    This reverts commit 4a090b2a4c5d2d6e3e1a3d18ff0d109de7eadf67.

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@
 #  last_sign_in_ip    :string
 #  mail               :string
 #  ou                 :string
-#  role               :integer          default("unassinged"), not null
+#  role               :integer          default(0), not null
 #  sign_in_count      :integer          default(0), not null
 #  sn                 :string
 #  uid                :string
@@ -30,6 +30,4 @@ class User < ApplicationRecord
   include EpiCas::DeviseHelper
 
   enum :role, [ :unassinged, :client, :operator, :admin, :owner ]
-  has_many :jobs, dependent: :destroy
-
 end
