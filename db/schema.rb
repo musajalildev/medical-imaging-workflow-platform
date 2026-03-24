ee4bd61b728907225cf8f60ea2203ca31b88b230
Benjamin Grassie <bdgrassie1@sheffield.ac.uk>
2026-03-24 13:33:55 +0000

    dropped canclled and completed jobs tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,24 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_03_23_124638) do
+ActiveRecord::Schema[8.0].define(version: 2026_03_24_133214) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
-
-  create_table "cancelled_jobs", force: :cascade do |t|
-    t.bigint "job_id", null: false
-    t.text "cancel_reason"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["job_id"], name: "index_cancelled_jobs_on_job_id"
-  end
-
-  create_table "complete_jobs", force: :cascade do |t|
-    t.bigint "job_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["job_id"], name: "index_complete_jobs_on_job_id"
-  end
 
   create_table "delayed_jobs", force: :cascade do |t|
     t.integer "priority", default: 0, null: false
@@ -127,8 +112,6 @@ ActiveRecord::Schema[8.0].define(version: 2026_03_23_124638) do
     t.index ["username"], name: "index_users_on_username"
   end
 
-  add_foreign_key "cancelled_jobs", "jobs"
-  add_foreign_key "complete_jobs", "jobs"
   add_foreign_key "image_files", "jobs"
   add_foreign_key "job_status_histories", "jobs"
   add_foreign_key "job_status_histories", "users", column: "initiator_id"
