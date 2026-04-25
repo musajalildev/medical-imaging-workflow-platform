3f8e0e5320c9d522a74d732438f3a6d6f2c3ef73
Benjamin Grassie <bdgrassie1@sheffield.ac.uk>
2026-04-25 16:02:56 +0100

    Revoked link for admin to 'manage' job in jobs/index.haml as it violated jobs controller constraints

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -5,7 +5,7 @@ class JobsController < ApplicationController
   load_and_authorize_resource
   MAX_FILE_SIZE_BYTES = 1_073_741_824 # 1 GB
   before_action :set_job, only: %i[ show edit update destroy upload_output update_status self_assign complete_job cancel_job ]
-  before_action :check_client_role, only: %i[ new create update ]
+  before_action :check_client_role, only: %i[ new create edit update ]
 
   # GET /jobs
   def index
