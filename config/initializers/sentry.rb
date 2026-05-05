387e955653e6aa5f914f658656c72628d63dde0d
Your name <youremail@sheffield.ac.uk>
2026-05-05 16:09:36 +0100

    set deployment configurations

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,7 +1,7 @@
 Sentry.init do |config|
   # Sentry is only enabled when the dsn is set.
   unless Rails.env.development? || Rails.env.test?
-    config.dsn = 'REPLACE_WITH_YOUR_DSN'
+    config.dsn = 'https://7d3417409d9012da8095c33bb18582c6@sentry.shefcompsci.org.uk/43'
   end
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
   config.before_send = -> (event, hint) { ActiveSupport::ParameterFilter.new(Rails.application.config.filter_parameters).filter(event.to_hash) }
