577ffa056e382ccf3e104a8a374355cf78548b06
Benjamin Grassie <bdgrassie1@sheffield.ac.uk>
2026-05-13 17:29:25 +0100

    Added confirmation dialogue for operator reassignment

--- app/packs/scripts/update_operator_listener.js
+++ app/packs/scripts/update_operator_listener.js
@@ -6,8 +6,8 @@ document.addEventListener("turbo:load", () => {
   if (!CURRENT_OPERATOR_EMAIL || !OPERATOR_SELECT || !REASSIGN_BUTTON) return;
 
   const toggleReassignButton = () => {
-    const selectedEmail = OPERATOR_SELECT.options[OPERATOR_SELECT.selectedIndex].text;
-    if (selectedEmail !== CURRENT_OPERATOR_EMAIL.textContent) {
+    const SELECTED_EMAIL = OPERATOR_SELECT.options[OPERATOR_SELECT.selectedIndex].text;
+    if (SELECTED_EMAIL !== CURRENT_OPERATOR_EMAIL.textContent) {
       REASSIGN_BUTTON.style.visibility = "visible";
     } else {
       REASSIGN_BUTTON.style.visibility = "hidden";
@@ -20,12 +20,31 @@ document.addEventListener("turbo:load", () => {
   toggleReassignButton();
 });
 
-// fix the stuck button issue where the declining the turbo confirmation dialog would leave the button in a disabled state, this is a workaround to re-enable the button when the user cancels the confirmation dialog
+// add confirmation dialogue on the submitter
 document.addEventListener("click", (event) => {
   const button = event.target.closest("#update_operator_button");
   if (!button) return;
 
-  setTimeout(() => {
-    button.disabled = false;
-  }, 100);
+  event.preventDefault();
+
+  const form = button.closest("form");
+
+  const OPERATOR_SELECT = document.getElementById("operator_select");
+  const SELECTED_EMAIL = OPERATOR_SELECT.options[OPERATOR_SELECT.selectedIndex].text;
+  const CURRENT_OPERATOR_EMAIL = document.getElementById("display_operator_email")?.textContent.trim();
+
+  let confirmationMessage = "You are about to unassign " + CURRENT_OPERATOR_EMAIL + " from this job, meaning they will no longer be able to work on it.\n";
+  switch (SELECTED_EMAIL) {
+    case "No Operator":
+      confirmationMessage += "\nAre you sure?";
+    break;
+    default:
+      confirmationMessage += "You will then assign " + SELECTED_EMAIL + " as the new operator.\n\nAre you sure?";
+    break;
+  }
+
+  const confirmed = window.confirm(confirmationMessage);
+  if (confirmed) {
+    form.requestSubmit(button);
+  }
 });
